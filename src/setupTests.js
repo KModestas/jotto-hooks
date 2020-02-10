4f7c954b21abd472dfbb7524ebe4b822e7cd663a
Modestas <modestask97@gmail.com>
2020-02-10 12:37:15 +0000

    initial commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+// this setupTests file runs before any test file, need to set up enzyme adapter here
+// CRA configures jest to look for this file name out of the box
+// it seems that setupTests needs to be in the root of your src, and tests need to be within src for them to work
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+configure({
+	adapter: new Adapter()
+})
