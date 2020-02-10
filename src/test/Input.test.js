b8d02eba6870e77eb15614d5f885b7bbd7022041
Modestas <modestask97@gmail.com>
2020-02-10 18:14:37 +0000

    input prop type test

--- src/test/Input.test.js
+++ src/test/Input.test.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { findByTestAttr } from './testUtils'
+import { findByTestAttr, checkProps } from './testUtils'
 import Input from '../components/Input'
 
-const setup = () => {
-	return shallow(<Input />)
+const setup = (secretWord = 'party') => {
+	return shallow(<Input secretWord={secretWord} />)
 }
 
 test('Input renders without error', () => {
 	const wrapper = setup()
 	const input = findByTestAttr(wrapper, 'component-input')
 	expect(input.length).toBe(1)
+})
+
+test('Does not throw warning with expected props ', () => {
+	checkProps(Input, { secretWord: 'party' })
 })
