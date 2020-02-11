01222a019ea6affc9812f0002ae9763909ff2b39
Modestas <modestask97@gmail.com>
2020-02-11 13:11:44 +0000

    input form

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,7 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Input = ({ secretWord }) => {
-	return <div data-test='component-input'></div>
+	// should not destructure hooks from React for test mocks to work
+	const [currentGuess, setCurrentGuess] = React.useState('')
+	return (
+		<form className='form-inline'>
+			<input
+				data-test='input-box'
+				className='mb-2 mx-sm-3'
+				type='text'
+				placeholder={''}
+				onChange={e => setCurrentGuess(e.target.value)}
+				value={currentGuess}
+			/>
+			<button data-test='submit-button' className='btn btn-primary mb-2'>
+				submit
+			</button>
+		</form>
+	)
 }
 
 Input.propTypes = {
