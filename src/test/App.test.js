b72541ef8b3d3e99f707f8fdcd0dc90b1547b78c
Modestas <modestask97@gmail.com>
2020-02-17 13:44:07 +0000

    getSecretWord test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,10 +3,16 @@ import { mount } from 'enzyme'
 import { findByTestAttr } from './testUtils'
 import App from '../components/App'
 
-// scoping globally
+import hookActions from '../redux/actions/hookActions'
+
 const mockSecretWord = jest.fn()
 
 const setup = () => {
+	// before each test
+	// clear mock function
+	mockSecretWord.mockClear()
+	// replace the real function with mock
+	hookActions.secretWord = mockSecretWord
 	// using mount instead of shallow which is not yet usable with useEffect
 	return mount(<App success={false} guessedWords={[{ guessedWord: 'yo', letterMatchCount: 0 }]} />)
 }
@@ -15,4 +21,12 @@ test('App renders without error', () => {
 	const wrapper = setup()
 	const component = findByTestAttr(wrapper, 'component-app')
 	expect(component.length).toBe(1)
+})
+
+describe('getSecretWord', () => {
+	test('is called on app Mount', () => {
+		setup()
+		expect(mockSecretWord).toHaveBeenCalled()
+	})
+	test('is called on app Mount', () => {})
 })
