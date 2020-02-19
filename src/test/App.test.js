b1af4612ff4a476361e02abc0247ed742ae7d20d
Modestas <modestask97@gmail.com>
2020-02-19 10:12:45 +0000

    spinner tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -7,12 +7,18 @@ import hookActions from '../redux/actions/hookActions'
 
 const mockSecretWord = jest.fn()
 
-const setup = () => {
+const setup = (secretWord = 'party') => {
 	// before each test
 	// clear mock function
 	mockSecretWord.mockClear()
+
 	// replace the real function with mock
 	hookActions.getSecretWord = mockSecretWord
+
+	// create mock useReducer and replace
+	const mockUseReducer = jest.fn().mockReturnValue([{ secretWord }, jest.fn()])
+	React.useReducer = mockUseReducer
+
 	// using mount instead of shallow which is not yet usable with useEffect
 	return mount(<App success={false} guessedWords={[{ guessedWord: 'yo', letterMatchCount: 0 }]} />)
 }
@@ -35,5 +41,35 @@ describe('getSecretWord', () => {
 		// for useEffect .update doesnt work, use setProps instead
 		wrapper.setProps()
 		expect(mockSecretWord).not.toHaveBeenCalled()
+	})
+})
+
+describe('setSecretWord is not null', () => {
+	let wrapper
+	beforeEach(() => {
+		wrapper = setup('party')
+	})
+	test('renders app ', () => {
+		const appComponent = findByTestAttr(wrapper, 'component-app')
+		expect(appComponent.exists()).toBe(true)
+	})
+	test('does not render spinner ', () => {
+		const spinnerComponent = findByTestAttr(wrapper, 'spinner')
+		expect(spinnerComponent.exists()).toBe(false)
+	})
+})
+
+describe('setSecretWord is null', () => {
+	let wrapper
+	beforeEach(() => {
+		wrapper = setup(null)
+	})
+	test('does not render app ', () => {
+		const appComponent = findByTestAttr(wrapper, 'component-app')
+		expect(appComponent.exists()).toBe(false)
+	})
+	test('renders spinner ', () => {
+		const spinnerComponent = findByTestAttr(wrapper, 'spinner')
+		expect(spinnerComponent.exists()).toBe(true)
 	})
 })
