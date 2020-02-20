57882ada946c5503152ed95ef4d0deec6bcf858d
Modestas <modestask97@gmail.com>
2020-02-20 10:38:21 +0000

    language context

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import hookActions from './redux/actions/hookActions'
+import languageContext from './contexts/languageContext'
+
 import Congrats from './components/Congrats'
 import GuessedWords from './components/GuessedWords'
-import hookActions from './redux/actions/hookActions'
 import Input from './components/Input'
+import languagePicker, { LanguagePicker } from './components/LanguagePicker'
 
 const reducer = (state, action) => {
 	switch (action.type) {
 		case 'setSecretWord':
 			return { ...state, secretWord: action.payload }
+		case 'setLanguage':
+			return { ...state, language: action.payload }
 		default:
 			throw new Error(`invalid action type: ${action.type}`)
 	}
 }
 
 const App = props => {
-	const [state, dispatch] = React.useReducer(reducer, { secretWord: null })
+	const [state, dispatch] = React.useReducer(reducer, { secretWord: null, language: 'en' })
 
 	const setSecretWord = secretWord => dispatch({ type: 'setSecretWord', payload: secretWord })
+	const setLanguage = secretWord => dispatch({ type: 'setLanguage', payload: language })
 
 	React.useEffect(() => {
 		hookActions.getSecretWord(setSecretWord)
@@ -35,7 +41,10 @@ const App = props => {
 	return (
 		<div className='container' data-test='component-app'>
 			<h1>Jotto</h1>
-			<Input secretWord={state.secretWord} />
+			<languageContext.Provider value={state.language}>
+				<LanguagePicker setLanguage={setLanguage} />
+				<Input secretWord={state.secretWord} />
+			</languageContext.Provider>
 		</div>
 	)
 }
