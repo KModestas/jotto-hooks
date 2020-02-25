e39de0d967bebe90241b0b4a727ef5e7c9f5ecbe
Modestas <modestask97@gmail.com>
2020-02-25 12:03:06 +0000

    intergration test setup

--- src/components/Input.js
+++ src/components/Input.js
@@ -37,8 +37,6 @@ const Input = ({ secretWord }) => {
 	)
 }
 
-Input.propTypes = {
-	secretWord: PropTypes.string.isRequired
-}
+Input.propTypes = { secretWord: PropTypes.string.isRequired }
 
 export default Input
